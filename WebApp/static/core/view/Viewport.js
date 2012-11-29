cbeea5b10b29d859e7e5f290634ee36750e36447
Nikola Lazovski <nikola.lazovski@gmail.com>
2012-11-29 17:37:05 +0100

    fix

--- WebApp/static/core/view/Viewport.js
+++ WebApp/static/core/view/Viewport.js
@@ -1,10 +1,10 @@
 Ext.define('DIRACWebFramework.view.Viewport', {
 	extend : 'Ext.container.Viewport',
-	uses : [ 'DIRACWebFramework.view.DiracDesktop' ],
+	uses : [ 'DIRACWebFramework.view.App' ],
 	layout : 'fit',
 	id : 'diracviewport',
 	initComponent : function() {
-		var desktop = new DIRACWebFramework.view.DiracDesktop();
+		var desktop = new DIRACWebFramework.view.App();
 		this.callParent(arguments);
 	}
 });
